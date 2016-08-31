af405e8d2785ea6accf677d699b988d81d5bfca8
Lukas Gartmair <lukas_gartmair@gmx.de>
2016-08-31 14:39:10 +0200

    cube mesh is loaded properly

--- lpcvt_functions.cpp
+++ lpcvt_functions.cpp
@@ -64,6 +64,7 @@ namespace Geex {
 	
 	// initialize a new vector which holds the new vertices of the delaunay triangulation
 	int number_of_vertices = RVD.delaunay()->nb_vertices();
+	/*
 	int xyzs = 3;
 	std::vector<std::vector<float> > cvt_vertices(number_of_vertices, std::vector<float>(xyzs));
 	
@@ -72,91 +73,10 @@ namespace Geex {
 	std::vector<std::vector<float> > cvt_triangles(number_of_triangles, std::vector<float>(number_of_vertex_indices_per_triangle));
 	
 	write_RDT(RVD, cvt_vertices, cvt_triangles) ;
+	*/
 	delete delaunay ;
 	return number_of_vertices;
 	}
-	
-//===================================================================================================
-//
-// Algebra tests
-//
-//  Computes :
-//     F_{L_p} in the surface case
-//  
-//
-//=================================================================================================== 
-
-
-    /**
-     * Gets the combinatorics of the integration simplices,
-     * i.e. 10 integers per integration simplex.
-     * (see Section 3.1 in the paper)
-     * Returns also the array of C vertices (three per integration simplex).
-     * Since they are easy to get during the combinatorial phase, they are
-     * computed here and kept for the algebraic phase.
-     *
-     * In 2D mode (volume = false), returns also the array F.
-     *   F[i] indicates the facet that contains the i-th integration simplex.
-     *
-     */
-    void get_combinatorics(
-        Mesh* M, const std::vector<vec3>& pts, 
-        std::vector<int>& I, std::vector<vec3>& C, std::vector<int>& F, bool volume
-    ) {
-        Delaunay* delaunay = Delaunay::create("CGAL") ;
-        delaunay->set_vertices(pts) ;
-            RestrictedVoronoiDiagram RVD(delaunay,M) ;
-            RVD.set_symbolic(true) ;
-            RVD.for_each_triangle(MemorizeIndicesAndFacets(RVD,I,C,F)) ;
-        
-        delete delaunay ;
-    }
-
-    /**
-     * Computes F_{L_p} and its gradient.
-     */
-    void compute_F_g(Mesh* m, const std::vector<vec3>& pts, unsigned int p, bool volume) {
-        std::cerr << "nb pts = " << pts.size() << "   nb facets = " << m->nb_facets() << std::endl ;
-        std::vector<int> I ;
-        std::vector<vec3> C ;
-        std::vector<int> F ;
-        get_combinatorics(m, pts, I, C, F, volume) ;
-        unsigned int nb_integration_simplices = (unsigned int)I.size() / 10 ;
-        std::vector<mat3> M(nb_integration_simplices) ;
-        for(unsigned int i=0; i<M.size(); i++) {
-            M[i].load_identity() ; 
-                // or replace with anisotropy field
-                //   In 2D: use F[i] to retreive the index of the facet that contains
-                //      the current integration simplex (and access an array of per-facet anisotropy).
-                //   In 3D: use geometric search from the centroid of the current
-                //      integration simplex.
-        }
-        std::vector<plane3> Q(m->nb_facets()) ;
-        for(unsigned int i=0; i<m->nb_facets(); i++) {
-            Q[i] = m->facet_plane(i) ;
-        }
-        std::vector<double> g(pts.size() * 3) ;
-        double f = compute_F_Lp(volume, p, m, I, C, pts, Q, M, g) ;
-        double gnorm = 0.0 ;
-        for(unsigned int i=0; i<g.size(); i++) {
-            gnorm += g[i]*g[i] ;
-        }
-        gnorm = ::sqrt(gnorm) ;
-        std::cerr.precision(16);
-        std::cerr << (volume ? "volume " : "surface ") 
-                  << "F_L" << p << ":" 
-                  << "f=" << std::scientific << f << "  g=" << gnorm << std::endl ;
-    }
-
-    void test_algebra(std::vector<std::vector<float> > initial_mesh_vertices, std::vector<std::vector<float> > triangles) 
-    {
-        Mesh M ;
-        unsigned int nb_borders = M.receiveVerticesAndTriangles(initial_mesh_vertices, triangles);
-        std::vector<vec3> pts ;
-        create_pts(initial_mesh_vertices, pts);
-        std::cerr << "          ========== surface LpCVT test ======" << std::endl ;
-        compute_F_g(&M, pts, 4, false) ;
-    }
 
     
 std::vector<std::vector<float> > initializeCubeVertices(float xmin, float ymin, float zmin)
@@ -165,37 +85,37 @@ std::vector<std::vector<float> > initializeCubeVertices(float xmin, float ymin, 
 	int xyz = 3;
 	std::vector<std::vector<float> > vertices(number_of_vertices, std::vector<float>(xyz));
 
-	vertices[0][0] = xmin;
+	vertices[0][0] = xmin+1;
 	vertices[0][1] = ymin;
 	vertices[0][2] = zmin;
 
 	vertices[1][0] = xmin+1;
 	vertices[1][1] = ymin;
-	vertices[1][2] = zmin;
+	vertices[1][2] = zmin+1;
 
-	vertices[2][0] = xmin+1;
-	vertices[2][1] = ymin+1;
-	vertices[2][2] = zmin;
+	vertices[2][0] = xmin;
+	vertices[2][1] = ymin;
+	vertices[2][2] = zmin+1;
 
 	vertices[3][0] = xmin;
-	vertices[3][1] = ymin+1;
+	vertices[3][1] = ymin;
 	vertices[3][2] = zmin;
 
-	vertices[4][0] = xmin;
-	vertices[4][1] = ymin;
-	vertices[4][2] = zmin+1;
+	vertices[4][0] = xmin+1;
+	vertices[4][1] = ymin+1;
+	vertices[4][2] = zmin;
 
 	vertices[5][0] = xmin+1;
-	vertices[5][1] = ymin;
+	vertices[5][1] = ymin+1;
 	vertices[5][2] = zmin+1;
 
-	vertices[6][0] = xmin+1;
+	vertices[6][0] = xmin;
 	vertices[6][1] = ymin+1;
 	vertices[6][2] = zmin+1;
 
 	vertices[7][0] = xmin;
 	vertices[7][1] = ymin+1;
-	vertices[7][2] = zmin+1;
+	vertices[7][2] = zmin;
 	
 	return vertices;
 }
